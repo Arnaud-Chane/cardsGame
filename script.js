e984e22d86dd26535c07598337b4ee7a7be0ce92
Arnaud-Chane <59416180+Arnaud-Chane@users.noreply.github.com>
2022-04-27 22:04:21 +0200

    Add files via upload
    First Upload
    I'll need to create 6 arrays
    Cards will be sorted between 4 of them, each for its color
    1 array for the card Top and Bottom
    And the last one will have every cards to shuffle them all

---
+++ script.js
@@ -0,0 +1,100 @@
+var arrClubs = [];
+var arrDiamonds = [];
+var arrHearts = [];
+var arrSpades = [];
+var arrDeck = [];
+var arrJoker = [];
+
+var arrAllCards = [];
+
+var firstEmptyCard = "";
+var idNb = 0;
+var id = "";
+var card = {};
+
+
+// Clubs in Array
+function mvClubsInArr() {
+	idNb++;
+	id = "card" + idNb;
+  arrClubs.push(firstEmptyCard);
+  card
+  console.log(arrClubs[0]);
+}
+
+mvClubsInArr();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
